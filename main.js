aee7fe0eb7ea1bf2f773c62f812b7de16206008a
kot14 <43212098+kot14@users.noreply.github.com>
2019-04-04 00:03:57 +0300

    complete logic

--- main.js
+++ main.js
@@ -1,44 +1,62 @@
-var app = angular.module('index',['ngRoute']);
-app.config(['$routeProvider','$locationProvider',function($routeProvider){
+var app = angular.module('index', ['ngRoute']);
 
-$routeProvider
-    .when('/home',{
-        templateUrl:'app/navigation/shopping-list/shopping-list.html',
-    })
+app.config(['$locationProvider', function ($locationProvider) {
+    $locationProvider.hashPrefix('');
+}]);
 
-    .when('/cart',{
-        templateUrl:'app/navigation/cart/cart.html',
-    })
-    .otherwise({
-        redirectTo:'/home'
-    });
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider) {
+
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'app/navigation/shopping-list/shopping-list.html',
+        })
+
+        .when('/cart', {
+            templateUrl: 'app/navigation/cart/cart.html',
+        })
+        .otherwise({
+            redirectTo: '/home'
+        });
 
 
 }]);
 
-app.controller('main',function(){
+app.controller('main', function ($location) {
+    this.cartStatus = true;
+    this.cartCounter = 0;
+
     this.items = [
-		{text: 'Iphone', price:'400$',available: true},
-		{text: 'Samsung', price:'300$',available: false},
-		{text: 'Xiaomi', price:'200$',available: true}
+        { text: 'Iphone', price: '400$', available: true },
+        { text: 'Samsung', price: '300$', available: false },
+        { text: 'Xiaomi', price: '200$', available: true }
     ];
     this.cartItems = [];
 
-this.add = function($index){  
-    this.cartItems.push(this.items[$index]);
-};
-this.ss = function(){
-    localStorage.setItem("priceList",JSON.stringify(this.cartItems) ); 
-}
+    this.goHome = function () {
+        $location.path('/home')
+    }
 
-this.getCart = function(){  
-    let json = localStorage.getItem("priceList"); 
-    data = JSON.parse(json)
-    this.cartItems = data;
-    console.log(this.cartItems);
-}
-this.clear = function($index){
-    this.cartItems.splice($index,1);
-}
+    this.add = function ($index) {
+        this.cartItems.push(this.items[$index]);
+        localStorage.setItem("priceList", JSON.stringify(this.cartItems));
+        this.cartStatus = false;
+        this.cartCounter = this.cartItems.length;
+    };
+
+    this.getCart = function () {
+        $location.path('/cart');
+        let json = localStorage.getItem("priceList");
+        data = JSON.parse(json)
+        this.cartItems = data;
+    }
+    this.clear = function ($index) {
+        this.cartCounter --
+        this.cartItems.splice($index, 1);
+        localStorage.setItem("priceList", JSON.stringify(this.cartItems));
+        if(this.cartItems.length <= 0){
+            $location.path('/home')
+            this.cartStatus = true;
+        };
+    }
 });
 
